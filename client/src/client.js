460438591b63d158b4a9298ba3869e709ff2a397
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-19 09:43:04 +0300

    added apollo client

--- client/src/client.js
+++ client/src/client.js
@@ -6,3 +6,28 @@ import gql from 'graphql-tag'
 /**
  * Create a new apollo client and export as default
  */
+
+
+const link = new HttpLink({ uri: 'https://rickandmortyapi.com/graphql' });
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    link,
+    cache
+});
+
+const query = gql`
+    {
+        characters {
+            results{
+              name
+            }
+          }
+    }
+`
+
+client.query({ query })
+    .then(result => console.log(result))
+
+export default client;
