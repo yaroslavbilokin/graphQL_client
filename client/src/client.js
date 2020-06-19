557f9d28ad5c443651ad43f0e88872aaa38a2ef7
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-19 11:24:01 +0300

    added query and mutation

--- client/src/client.js
+++ client/src/client.js
@@ -7,8 +7,7 @@ import gql from 'graphql-tag'
  * Create a new apollo client and export as default
  */
 
-
-const link = new HttpLink({ uri: 'https://rickandmortyapi.com/graphql' });
+const link = new HttpLink({ uri: 'http://localhost:4000/' });
 
 const cache = new InMemoryCache();
 
@@ -16,18 +15,5 @@ const client = new ApolloClient({
     link,
     cache
 });
-
-const query = gql`
-    {
-        characters {
-            results{
-              name
-            }
-          }
-    }
-`
-
-client.query({ query })
-    .then(result => console.log(result))
 
 export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,19 +1,52 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
 
-export default function Pets () {
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+}
+`
+
+export default function Pets() {
   const [modal, setModal] = useState(false)
-
+  const { data, loading, error } = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(NEW_PET)
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: { newPet: input }
+    })
   }
-  
+
+  if (loading || newPet.loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <p>error!</p>
+  }
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +65,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
